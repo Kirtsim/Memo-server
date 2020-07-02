630142c046d0120c30fd17130869c03112504ba5
Kirtsim <filipmistrik@gmail.com>
2020-07-02 09:04:31 +0200

    define Request and Header

---
+++ src/memo/Header.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+namespace memo {
+
+struct Header
+{
+    std::string _name;
+    std::string _value;
+};
+
+} // namespace memo
+
---
+++ src/memo/Reply.cpp
@@ -0,0 +1,1 @@
+#include "memo/Reply.hpp"
