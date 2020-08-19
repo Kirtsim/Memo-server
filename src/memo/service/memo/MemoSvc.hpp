c351489d1349406e796809a52b83e3a09830f8e0
Kirtsim <filipmistrik@gmail.com>
2020-08-19 18:16:47 +0200

    Refactor MemoSvc and TagSvc into BaseService

--- src/memo/service/memo/MemoSvc.hpp
+++ src/memo/service/memo/MemoSvc.hpp
@@ -1,10 +1,6 @@
 #pragma once
-#include "memo/service/Service.hpp"
-#include "memo/service/Process.hpp"
+#include "memo/service/BaseService.hpp"
 #include "model/MemoSvc.grpc.pb.h"
-
-#include <grpcpp/impl/codegen/completion_queue.h>
-#include <grpcpp/impl/codegen/async_unary_call_impl.h>
 
 #include <unordered_map>
 
@@ -18,10 +14,8 @@ class CreateProcess;
 class DeleteProcess;
 class UpdateProcess;
 
-;
-
 class MemoSvc : public model::MemoSvc::AsyncService,
-                public memo::service::Service
+                public memo::service::BaseService
 {
 public:
     MemoSvc(const std::shared_ptr<Resources>& ioResources, grpc::ServerCompletionQueue& ioCompletionQueue);
@@ -51,21 +45,8 @@ public:
                         const model::Id* request,
                         model::OperationStatus* response) override;
 
-    /// Service interface
-    bool executeProcess(Process* process) override;
-
-    void enable() override;
-
-    void disable() override;
-
-    int getId() const override;
-
-private:
-    void registerProcess(Process::Ptr iProcess);
-
-    std::shared_ptr<Resources> resources_;
-    grpc::ServerCompletionQueue& completionQueue_;
-    std::unordered_map<Process*, Process::Ptr> processes_;
+    /// BaseService methods
+    void registerProcesses() override;
 };
 
 } // namespace service
