6ee9cbeb69806e97381b79758c7472c036dbed95
Filip Mistrik <filipmistrik@gmail.com>
2021-08-23 21:22:57 +0100

    Modify logger formatting.

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -1,19 +1,17 @@
 #include "logger/logger.hpp"
 
-namespace memo {
-namespace logger {
+namespace memo::logger {
 
 
 void Init(const std::string& iLogFilePath)
 {
     el::Configurations config;
     config.setToDefault();
-    config.setGlobally(el::ConfigurationType::Format, "%datetime %levshort <%fbase:%line> %msg");
+    config.setGlobally(el::ConfigurationType::Format, "[%datetime{%d/%M/%Y %H:%m:%s}] %levshort %fbase:%line %msg");
     config.setGlobally(el::ConfigurationType::Filename, iLogFilePath);
     el::Loggers::reconfigureLogger("default", config);
     el::Loggers::addFlag(el::LoggingFlag::ImmediateFlush);
     el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
 }
 
-} // namespace logger
-} // namespace memo
+} // namespace memo::logger
