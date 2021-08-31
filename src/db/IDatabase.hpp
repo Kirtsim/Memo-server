1c701444e2926767adb71504897a257a9740f91a
Filip Mistrik <filipmistrik@gmail.com>
2021-08-31 19:30:42 +0100

    Add documentation to the IDatabase interace.

--- src/db/IDatabase.hpp
+++ src/db/IDatabase.hpp
@@ -17,10 +17,24 @@ class IDatabase
 public:
     virtual ~IDatabase() = default;
 
+    /// @brief Opens the database for writing, brings it to a state where its data can be queried and
+    ///        modified.
+    /// @return true if the Database was open, false otherwise.
     virtual bool open() = 0;
 
+    /// @brief Closes the database, deallocates resources. If successful, the database cannot be
+    ///        modified or queried.
+    /// @return true if the database was closed successfully, false otherwise.
     virtual bool close() = 0;
 
+    /// @brief Executes a give query and calls the callback function once a result is available.
+    ///        In case of a search query, the callback function is called for each row in the
+    ///        table returned by the query.
+    ///        In case of a non-search query, such as CREATE, UPDATE or DELETE, the callback
+    ///        function is only called once.
+    /// @param query string containing query to be executed.
+    /// @param callback callback function that will be called for each result returned.
+    /// @return true if execution was successful, false otherwise.
     virtual bool exec(const std::string& query, const SQLCallback& callback) = 0;
 };
 
