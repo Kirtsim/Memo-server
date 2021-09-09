52c7f8117d9100df2aa3d575488bf2ba2020a5cb
Filip Mistrik <filipmistrik@gmail.com>
2021-09-09 21:34:58 +0100

    Update doc string of a callback function.

--- src/db/IDatabase.hpp
+++ src/db/IDatabase.hpp
@@ -9,7 +9,7 @@ namespace memo {
 // Callback function that will be called for each queried row.
 // param [0] - queried row values
 // param [1] - queried row column names
-// return - True if the query evaluation should continue, false if it should be aborted.
+// return - true if the query evaluation should stop, false if it should continue evaluating.
 using SQLCallback = std::function<bool(const std::vector<std::string>&, const std::vector<std::string>&)>;
 
 class IDatabase
