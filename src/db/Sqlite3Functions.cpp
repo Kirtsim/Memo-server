3c1fd30d256629f546cef880da3bf71c3c42171f
Filip Mistrik <filipmistrik@gmail.com>
2021-09-14 19:31:15 +0100

    Add function for updating a Tag through sqlite3.

--- src/db/Sqlite3Functions.cpp
+++ src/db/Sqlite3Functions.cpp
@@ -4,7 +4,15 @@
 #include "model/Memo.hpp"
 #include <sstream>
 
-namespace memo{
+namespace memo {
+
+namespace {
+
+int ColorToInt(const model::Color& color);
+
+model::Color IntToColor(int numericColor);
+
+} // namespace
 
 bool UpdateMemoTable(const model::Memo& memo, ISqlite3Wrapper& sqlite3)
 {
@@ -17,6 +25,21 @@ bool UpdateMemoTable(const model::Memo& memo, ISqlite3Wrapper& sqlite3)
     << " WHERE " << att::kId << "=" << memo.id() << ";";
 
     return sqlite3.exec(memoCmd.str(), nullptr);
+}
+
+bool UpdateTagTable(const model::Tag& tag, ISqlite3Wrapper& sqlite3)
+{
+    namespace att = TagTable::att;
+
+    std::stringstream updateCmd;
+    updateCmd << "UPDATE " << TagTable::kName
+              << " SET " << att::kName << "=" << tag.name() << ", "
+                         << att::kColor << "=" << ColorToInt(tag.color()) << ", "
+                         << att::kTimestamp << "=" << tag.timestamp()
+              << " WHERE " << att::kId << "=" << tag.id() << ";";
+
+    return sqlite3.exec(updateCmd.str(), nullptr);
+
 }
 
 bool SelectMemoTagIds(const model::Memo& memo, std::vector<unsigned long>& tagIds, ISqlite3Wrapper& sqlite3)
@@ -64,4 +87,19 @@ bool DeleteMemoTagIds(const model::Memo& memo, const std::vector<unsigned long>&
     return sqlite3.exec(deleteCmd.str(), nullptr);
 }
 
+namespace {
+
+int ColorToInt(const model::Color& color)
+{
+    // TODO:  implement
+    return 0;
+}
+
+model::Color IntToColor(int numericColor)
+{
+    // TODO: implement
+    return { 0, 0, 0 };
+}
+
+} // namespace
 } // namespace memo
--- src/db/Sqlite3Functions.hpp
+++ src/db/Sqlite3Functions.hpp
@@ -11,6 +11,8 @@ namespace model { class Tag; }
 
 bool UpdateMemoTable(const model::Memo& memo, ISqlite3Wrapper& sqlite3);
 
+bool UpdateTagTable(const model::Tag& tag, ISqlite3Wrapper& sqlite3);
+
 bool SelectMemoTagIds(const model::Memo& memo, std::vector<unsigned long>& tagIds, ISqlite3Wrapper& sqlite3);
 
 bool InsertMemoTagIds(const model::Memo& memo, const std::vector<unsigned long>& tagIds, ISqlite3Wrapper& sqlite3);
